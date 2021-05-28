77271c8266a0a2be1d92150e0584abbd9c40d889
great-pacey <1144932382@qq.com>
2021-05-28 13:40:47 +0800

    Update aliexpress-search.js

--- aliexpress-search.js
+++ aliexpress-search.js
@@ -4,6 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
+// @updateURL    https://github.com/great-pacey/tampermonkey/blob/main/aliexpress-search.js
 // @match        *://www.aliexpress.com/*
 // @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
 // @grant        GM_setClipboard
